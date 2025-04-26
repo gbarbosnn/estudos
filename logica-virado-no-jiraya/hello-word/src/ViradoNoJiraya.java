a4e3f616f9e15a07a79d6c00b1b1f0152bf5bd9b
Gustavo gay <gustavogay@MacBook-Air-de-MacBook.local>
2025-04-26 12:31:11 -0300

    feat: adiciona exercicio proposto na aula 07

--- logica-virado-no-jiraya/hello-word/src/ViradoNoJiraya.java
+++
@@ -1,5 +0,0 @@
-public class ViradoNoJiraya {
-    public static void main(String[] args) {
-        System.out.println("Meu Deus, Meu Deus, eu sou um hacker!");
-    } // alias: psvm
-}
---
+++ logica-virado-no-jiraya/src/ViradoNoJiraya.java
@@ -0,0 +1,7 @@
+public class ViradoNoJiraya {
+    public static void main(String[] args) {
+        short idade = 23;
+        System.out.println(idade);
+        System.out.println("Minha idade é: "+idade);
+    } // alias: psvm
+}
