f8018c417dccd46110415fd9db6c71465e49326a
Gustavo <gubibash@gmail.com>
2025-04-27 15:15:50 -0300

    fix: adiciona descrição do exercicio

--- logica-virado-no-jiraya/src/CalculadorDiferencaIdade.java
+++ logica-virado-no-jiraya/src/CalculadorDiferencaIdade.java
@@ -1,6 +1,10 @@
+/*
+Criar classe para calcular diferença entre duas idades
+ */
+
 public class CalculadorDiferencaIdade {
     public static void main(String[] args) {
-        short primeiraIdade = 32;
+        short primeiraIdade = 40;
         short segundaIdade = 47;
 
         int diferencaDeIdades = primeiraIdade - segundaIdade;
