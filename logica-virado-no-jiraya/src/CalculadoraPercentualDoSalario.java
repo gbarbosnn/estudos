852bd90dbbd39dcb2c0d9420e2045245571e6259
Gustavo <gubibash@gmail.com>
2025-04-27 16:21:01 -0300

    feat: exercicio proposto aula 11

---
+++ logica-virado-no-jiraya/src/CalculadoraPercentualDoSalario.java
@@ -0,0 +1,13 @@
+/*
+Criar uma classe que calcule uma determinada porcentagem de um dado salário
+ */
+
+public class CalculadoraPercentualDoSalario {
+    public static void main(String[] args) {
+        float salario = 1450.50f;
+        float percentual = 0.25f;
+        float percentualDoSalario = salario * percentual;
+
+        System.out.println((percentual * 100) + "% de um salário de " + salario + " é igual a: "+ percentualDoSalario);
+    }
+}
