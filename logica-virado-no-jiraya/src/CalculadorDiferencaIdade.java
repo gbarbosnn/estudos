f7df76112b92403ca086fd169d43ff0f919c8e78
Gustavo gay <gustavogay@MacBook-Air-de-MacBook.local>
2025-04-27 15:05:21 -0300

    feat: adiciona exercicio proposto na aula 8

---
+++ logica-virado-no-jiraya/src/CalculadorDiferencaIdade.java
@@ -0,0 +1,9 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args) {
+        short primeiraIdade = 32;
+        short segundaIdade = 47;
+
+        int diferencaDeIdades = primeiraIdade - segundaIdade;
+        System.out.println("A diferençade de idade é: " + diferencaDeIdades + " anos");
+    }
+}
