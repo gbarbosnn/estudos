054a2e719ef4bea15ce9574a0b5bcadf0f654475
Gustavo <gubibash@gmail.com>
2025-04-27 21:08:40 -0300

    feat:adiciona exercicios da aula 12

---
+++ logica-virado-no-jiraya/src/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        char inicial = 'G';
+        String nome = "Gustavo";
+
+        System.out.println("A inicial é: " + inicial);
+        System.out.println("O nome é: " + nome);
+    }
+}
