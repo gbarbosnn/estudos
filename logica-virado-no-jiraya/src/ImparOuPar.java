1dd26197c05f503c5edea9d78b0ca412bd6865e0
gbarbosnn <144746208+gbarbosnn@users.noreply.github.com>
2025-04-29 22:50:44 -0300

    feat: adiciona exercicios aula 16 - pt 4

---
+++ logica-virado-no-jiraya/src/ImparOuPar.java
@@ -0,0 +1,11 @@
+public class ImparOuPar {
+    public static void main(String[] args) {
+        int numeroX = 3;
+
+        if ((numeroX %2) ==0){
+            System.out.println("Par");
+        }else{
+            System.out.println("Impar");
+        }
+    }
+}
