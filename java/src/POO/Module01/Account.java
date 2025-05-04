340e131b1c67b6647067131ef839851c67e79bb6
Gustavo Barbosa <gubibash@gmail.com>
2025-05-04 20:52:00 -0300

    feat: adiciona métodos e regras de negócios a conta

--- java/src/POO/Module01/Account.java
+++ java/src/POO/Module01/Account.java
@@ -6,18 +6,40 @@ public class Account {
     double balance;
 
     /**
-     * This method prints the account balance.
+     * Prints the current account balance to the console.
      */
-    void printBalance(){
-        System.out.println("Balance: R$ "+ balance);
+    void printBalance() {
+        System.out.println("Balance: R$ " + balance);
+    }
+
+
+    /**
+     * Performs a deposit into the account.
+     *
+     * @param amount The amount to deposit.
+     * @return true if the deposit is successful, and false if it is not.
+     */
+    boolean deposit(double amount){
+        if(amount < 0 ){
+            return false;
+        }
+
+        balance += amount;
+        return true;
+
     }
 
     /**
-     * This method makes a deposit into the account.
-     * @param amount This is the amount to deposit.
+     * The method performs a withdrawal from the account.
+     * @param amount The amount to withdraw.
+     * @return true if the withdrawal is successful, and false if it is not.
      */
-    void deposit(double amount){
-        balance += amount;
-    }
+    boolean withdraw(double amount){
+        if(amount > 0 && balance > 0 && amount <= balance){
+            balance-=amount;
+            return true;
+        }
 
+        return false;
+    }
 }
--- java/src/POO/Module01/App2.java
+++ java/src/POO/Module01/App2.java
@@ -4,10 +4,35 @@ public class App2 {
     public static void main(String[] args) {
         Account account = new Account();
 
-        account.deposit(100);
+        if(!account.deposit(-100)){
+            System.out.println("Ops, depósito não realizado");
+        };
+
         account.printBalance();
+        System.out.println("==================================");
 
         account.deposit(200);
         account.printBalance();
+        System.out.println("==================================");
+
+        if(!account.withdraw(-50)){
+            System.out.println("Ops, saque não realizado");
+        }
+        System.out.println("==================================");
+
+        account.withdraw(50);
+        account.printBalance();
+        System.out.println("==================================");
+
+        if(!account.withdraw(200)){
+            System.out.println("Ops, saque não realizado");
+        }
+        System.out.println("==================================");
+
+        if(!account.withdraw(150)){
+            System.out.println("Ops, saque não realizado");
+        }
+        account.printBalance();
+        System.out.println("==================================");
     }
 }
