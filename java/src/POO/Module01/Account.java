6c54154ee6b6e56c45749693cdaa6d6070a3f83b
Gustavo Barbosa <gubibash@gmail.com>
2025-05-04 19:11:19 -0300

    feat: introdução a POO

---
+++ java/src/POO/Module01/Account.java
@@ -0,0 +1,8 @@
+package POO.Module01;
+
+public class Account { //Classe -> Sempre com primeira letra maiuscula
+    String accountNumber; //Atributo -> Tipo e nome da variavel em camelCase
+    String accountOwner;
+    double balance;
+    // As classes definem as estruturas dos objetos da aplicacao
+}
---
+++ java/src/POO/Module01/App.java
@@ -0,0 +1,26 @@
+package POO.Module01;
+
+public class App {
+    public static void main(String[] args) {
+        Account accountGustavo = new Account();
+
+        accountGustavo.accountNumber = "1234-6";
+        accountGustavo.accountOwner = "Gustavo";
+        accountGustavo.balance = 200;
+
+        Account accountGabriel = new Account();
+
+        accountGabriel.accountNumber = "1235-6";
+        accountGabriel.accountOwner = "Gabriel";
+        accountGabriel.balance = 400;
+
+        System.out.println(accountGabriel.accountNumber);
+        System.out.println(accountGabriel.accountOwner);
+        System.out.println(accountGabriel.balance);
+        System.out.println("---------------------");
+        System.out.println(accountGustavo.accountNumber);
+        System.out.println(accountGustavo.accountOwner);
+        System.out.println(accountGustavo.balance);
+
+    }
+}
