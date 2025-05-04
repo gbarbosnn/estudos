f67aed30e471175b61ebd3b1b62d08fa2e372bd6
Gustavo Barbosa <gubibash@gmail.com>
2025-05-03 21:15:08 -0300

    feat: exemplos de `while` e `do while`

---
+++ java/src/BasicJava/RepeatWhileDoWhile.java
@@ -0,0 +1,24 @@
+package BasicJava;
+
+public class RepeatWhileDoWhile {
+    public static void main(String[] args) {
+        int number = 11;
+
+        System.out.println("While");
+        while(number <= 10){
+            System.out.println(number);
+
+            number++;
+        }
+        System.out.println("Fim");
+
+        int numberTwo = 11;
+        System.out.println("Do While");
+        do {
+            System.out.println(numberTwo);
+            numberTwo++;
+        }while (numberTwo<=10);
+
+        System.out.println("Fim");
+    }
+}
